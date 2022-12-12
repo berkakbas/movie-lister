14f522c82b9f789618c678ea6caf871b0bee17f6
akbasberk <53055798+berkakbas@users.noreply.github.com>
2022-12-12 16:22:22 +0300

    base fragments added & bottom navigation implemented

---
+++ app/src/main/java/com/example/movielister/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.movielister
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+private const val ARG_PARAM1 = "param1"
+
+class HomeFragment : Fragment() {
+    private var param1: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let { param1 = it.getString(ARG_PARAM1) }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
--- app/src/main/java/com/example/movielister/MainActivity.kt
+++ app/src/main/java/com/example/movielister/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.movielister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.movielister.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val homeFragment = HomeFragment()
+        val moviesFragment = MoviesFragment()
+        val searchFragment = SearchFragment()
+        val profileFragment = ProfileFragment()
+
+        setCurrentFragment(homeFragment)
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(homeFragment)
+                R.id.movies -> setCurrentFragment(moviesFragment)
+                R.id.search -> setCurrentFragment(searchFragment)
+                R.id.profile -> setCurrentFragment(profileFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
     }
 }
