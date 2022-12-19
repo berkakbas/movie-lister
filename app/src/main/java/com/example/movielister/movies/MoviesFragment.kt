b4e342467856cebb7a273c955d176d592ed83ac6
akbasberk <53055798+berkakbas@users.noreply.github.com>
2022-12-19 11:32:55 +0300

    RecyclerView & Adapter implemented

--- app/src/main/java/com/example/movielister/movies/MoviesFragment.kt
+++ app/src/main/java/com/example/movielister/movies/MoviesFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.movielister.adapter.MovieListAdapter
 import com.example.movielister.databinding.FragmentMoviesBinding
+import com.example.movielister.network.MoviesNetwork
 
 class MoviesFragment : Fragment() {
     private var _binding: FragmentMoviesBinding? = null
@@ -18,6 +20,10 @@ class MoviesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentMoviesBinding.inflate(inflater, container, false)
         val root = binding.root
+
+        val list = MoviesNetwork.fetchPopularMovies()
+        binding.moviesRecyclerView.adapter = MovieListAdapter(list)
+
         return root
     }
 }
