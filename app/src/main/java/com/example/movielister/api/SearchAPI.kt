2b34c0c37a1e822f7959d9d1112946d8c6ab532c
akbasberk <53055798+berkakbas@users.noreply.github.com>
2022-12-20 11:27:02 +0300

    Login, Search, Profile network structures fixed

--- app/src/main/java/com/example/movielister/api/SearchAPI.kt
+++ app/src/main/java/com/example/movielister/api/SearchAPI.kt
@@ -1,9 +1,9 @@
 package com.example.movielister.api
 
-import com.example.movielister.BuildConfig.*
+import com.example.movielister.BuildConfig.API_KEY
+import com.example.movielister.BuildConfig.API_STR
 import com.example.movielister.model.MovieModel
 import com.example.movielister.model.TvSeriesModel
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -11,9 +11,9 @@ const val SEARCH_MOVIE = "search/movie/"
 const val SEARCH_TV_SERIE = "search/tv/"
 
 interface SearchAPI {
-    @GET("$BASE_URL$SEARCH_MOVIE{query}$API_STR$API_KEY")
-    fun searchMovie(@Path("query") query: String): Call<List<MovieModel>>
+    @GET("$SEARCH_MOVIE{query}$API_STR$API_KEY")
+    fun searchMovie(@Path("query") query: String): List<MovieModel>
 
-    @GET("$BASE_URL$SEARCH_TV_SERIE{query}$API_STR$API_KEY")
-    fun searchTvSerie(@Path("query") query: String): Call<List<TvSeriesModel>>
+    @GET("$SEARCH_TV_SERIE{query}$API_STR$API_KEY")
+    fun searchTvSerie(@Path("query") query: String): List<TvSeriesModel>
 }
--- app/src/main/java/com/example/movielister/search/SearchViewModel.kt
+++ app/src/main/java/com/example/movielister/search/SearchViewModel.kt
@@ -1,6 +1,42 @@
 package com.example.movielister.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movielister.model.MovieModel
+import com.example.movielister.model.TvSeriesModel
+import com.example.movielister.network.SearchNetwork
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
-class SearchViewModel: ViewModel() {
+class SearchViewModel : ViewModel() {
+    private val searchService = SearchNetwork.createSearchAPI()
+
+    private val _movieResults = MutableSharedFlow<List<MovieModel>>()
+    val movieResults = _movieResults.asSharedFlow()
+
+    private val _seriesResults = MutableSharedFlow<List<TvSeriesModel>>()
+    val seriesResults = _seriesResults.asSharedFlow()
+
+    fun searchMovie(query: String) {
+        viewModelScope.launch {
+            try {
+                val movies = searchService.searchMovie(query)
+                _movieResults.emit(movies)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun searchTvSeries(query: String) {
+        viewModelScope.launch {
+            try {
+                val series = searchService.searchTvSerie(query)
+                _seriesResults.emit(series)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
