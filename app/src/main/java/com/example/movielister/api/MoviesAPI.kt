eec6f95554f88b482e4e82229ff9ca8810d0ba9a
akbasberk <53055798+berkakbas@users.noreply.github.com>
2022-12-20 11:00:13 +0300

    Flow fixes & json parsing handled with moshi library

--- app/src/main/java/com/example/movielister/api/MoviesAPI.kt
+++ app/src/main/java/com/example/movielister/api/MoviesAPI.kt
@@ -2,7 +2,6 @@ package com.example.movielister.api
 
 import com.example.movielister.BuildConfig.*
 import com.example.movielister.model.MovieModel
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -10,9 +9,9 @@ const val POPULAR_MOVIE = "movie/popular/"
 const val MOVIE = "movie/"
 
 interface MoviesAPI {
-    @GET(BASE_URL + POPULAR_MOVIE + API_STR + API_KEY)
-    fun fetchPopularMovies(): Flow<List<MovieModel>>
+    @GET(POPULAR_MOVIE + API_STR + API_KEY)
+    suspend fun fetchPopularMovies(): List<MovieModel>
 
-    @GET("$BASE_URL$MOVIE{id}$API_STR$API_KEY")
-    fun fetchMovie(@Path("id") movieId: Int): Flow<MovieModel>
+    @GET("$MOVIE{id}$API_STR$API_KEY")
+    suspend fun fetchMovie(@Path("id") movieId: Int): MovieModel
 }
--- app/src/main/java/com/example/movielister/movies/MoviesViewModel.kt
+++ app/src/main/java/com/example/movielister/movies/MoviesViewModel.kt
@@ -4,45 +4,38 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.movielister.model.MovieModel
 import com.example.movielister.network.MoviesNetwork
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 
 class MoviesViewModel() : ViewModel() {
-    private val moviesAPI= MoviesNetwork.createMoviesAPI()
+    private val moviesService = MoviesNetwork.createMoviesAPI()
 
     private val _popularMoviesList = MutableSharedFlow<List<MovieModel>>()
-    val popularMovieList = _popularMoviesList.asSharedFlow()
+    val popularMoviesList = _popularMoviesList.asSharedFlow()
 
     private val _currentMovie = MutableSharedFlow<MovieModel>()
     val currentMovie = _currentMovie.asSharedFlow()
 
     fun fetchPopularMovies() {
         viewModelScope.launch {
-            moviesAPI.fetchPopularMovies()
-                .flowOn(Dispatchers.IO)
-                .catch { e ->
-                    e.printStackTrace()
-                }
-                .collect {
-                    _popularMoviesList.emit(it)
-                }
+            try {
+                val popularMovies = moviesService.fetchPopularMovies()
+                _popularMoviesList.emit(popularMovies)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
     fun fetchMovie(movieID: Int) {
         viewModelScope.launch {
-            moviesAPI.fetchMovie(movieID)
-                .flowOn(Dispatchers.IO)
-                .catch { e ->
-                    e.printStackTrace()
-                }
-                .collect {
-                    _currentMovie.emit(it)
-                }
+            try {
+                val movie = moviesService.fetchMovie(movieID)
+                _currentMovie.emit(movie)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
