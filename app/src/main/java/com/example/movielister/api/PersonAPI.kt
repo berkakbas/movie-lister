73571c0154170cb0ef0c2b3dd092f4a1a5d0b130
akbasberk <53055798+berkakbas@users.noreply.github.com>
2022-12-20 11:27:34 +0300

    Login, Search, Profile network structure fix commit-2

--- app/src/main/java/com/example/movielister/api/PersonAPI.kt
+++ app/src/main/java/com/example/movielister/api/PersonAPI.kt
@@ -1,14 +1,13 @@
 package com.example.movielister.api
 
+import com.example.movielister.BuildConfig.*
 import com.example.movielister.model.PersonModel
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-import com.example.movielister.BuildConfig.*
 
 const val PERSON = "person/"
 
 interface PersonAPI {
-    @GET("$BASE_URL$PERSON{id}$API_STR$API_KEY")
-    fun fetchPersonInfo(@Path("id") id: Int): Call<PersonModel>
+    @GET("$PERSON{id}$API_STR$API_KEY")
+    fun fetchPersonInfo(@Path("id") id: Int): PersonModel
 }
--- app/src/main/java/com/example/movielister/network/SearchNetwork.kt
+++ app/src/main/java/com/example/movielister/network/SearchNetwork.kt
@@ -1,56 +1,9 @@
 package com.example.movielister.network
 
 import com.example.movielister.api.SearchAPI
-import com.example.movielister.model.MovieModel
-import com.example.movielister.model.TvSeriesModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 object SearchNetwork {
-    private fun createSearchAPI(): SearchAPI {
+    fun createSearchAPI(): SearchAPI {
         return WebService.createRetrofit().create(SearchAPI::class.java)
-    }
-
-    fun searchMovie(query: String): List<MovieModel> {
-        val call = createSearchAPI().searchMovie(query)
-        var searchResult = listOf<MovieModel>()
-
-        call.enqueue(
-            object : Callback<List<MovieModel>> {
-                override fun onFailure(call: Call<List<MovieModel>>, t: Throwable) {
-                    t.printStackTrace()
-                }
-
-                override fun onResponse(call: Call<List<MovieModel>>, response: Response<List<MovieModel>>) {
-                    if (response.isSuccessful) {
-                        response.body()?.let {
-                            searchResult = it
-                        }
-                    }
-                }
-            })
-        return searchResult
-    }
-
-    fun searchTvSerie(query: String): List<TvSeriesModel> {
-        val call = createSearchAPI().searchTvSerie(query)
-        var searchResult = listOf<TvSeriesModel>()
-
-        call.enqueue(
-            object : Callback<List<TvSeriesModel>> {
-                override fun onFailure(call: Call<List<TvSeriesModel>>, t: Throwable) {
-                    t.printStackTrace()
-                }
-
-                override fun onResponse(call: Call<List<TvSeriesModel>>, response: Response<List<TvSeriesModel>>) {
-                    if (response.isSuccessful) {
-                        response.body()?.let {
-                            searchResult = it
-                        }
-                    }
-                }
-            })
-        return searchResult
     }
 }
