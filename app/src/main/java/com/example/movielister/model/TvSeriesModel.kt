ed120614a9a3233e488d23fe49b9e6a8c4c142bc
akbasberk <53055798+berkakbas@users.noreply.github.com>
2022-12-20 11:02:45 +0300

    all model classes changed accr. to moshi

--- app/src/main/java/com/example/movielister/model/TvSeriesModel.kt
+++ app/src/main/java/com/example/movielister/model/TvSeriesModel.kt
@@ -1,12 +1,11 @@
 package com.example.movielister.model
 
-import kotlinx.serialization.Serializable
+import com.squareup.moshi.Json
 
-@Serializable
 data class TvSeriesModel(
-    val id: Int,
-    val name: String,
-    val overview: String,
-    val posterPath: String?,
-    val backdrop_path: String?
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "overview") val overview: String,
+    @Json(name = "posterPath") val posterPath: String?,
+    @Json(name = "backdrop_path") val backdrop_path: String?
 )
