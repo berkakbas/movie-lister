4296aaecd7eff2207658f526c40cebaf1261b94f
akbasberk <53055798+berkakbas@users.noreply.github.com>
2022-12-28 11:41:10 +0300

    movie data binded to details screen

--- app/src/main/java/com/example/movielister/movies/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/movielister/movies/MovieDetailsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.movielister.movies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.movielister.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.example.movielister.data.MovieGenreHelper
 import com.example.movielister.databinding.ActivityMovieDetailsBinding
+import com.example.movielister.helper.HelperFunctions
+import com.example.movielister.model.MovieModel
+import com.squareup.picasso.Picasso
 
 class MovieDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMovieDetailsBinding
@@ -13,5 +16,15 @@ class MovieDetailsActivity : AppCompatActivity() {
 
         binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val intent = intent
+        val movie = intent.getSerializableExtra("movie") as MovieModel
+
+        Picasso.get().load(movie.imageUrl + movie.posterPath).into(binding.movieImage)
+        binding.ratingText.text = movie.voteAverage.toString()
+        binding.movieTitle.text = movie.title
+        binding.genreText.text = MovieGenreHelper.genreIdsToString(movie.genreIds)
+        binding.dateText.text = HelperFunctions.organizeDate(movie.releaseDate)
+        binding.descriptionText.text = movie.overview
     }
 }
