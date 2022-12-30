e871cfddffc353ceca2cd3b9c9afdcbcddb8f7ac
akbasberk <53055798+berkakbas@users.noreply.github.com>
2022-12-30 16:03:06 +0300

    replace loginContainerView from ProfileFragment

--- app/src/main/java/com/example/movielister/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/movielister/profile/ProfileFragment.kt
@@ -1,13 +1,14 @@
 package com.example.movielister.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.movielister.R
 import com.example.movielister.databinding.FragmentProfileBinding
+import com.example.movielister.main.MainActivity
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -21,7 +22,14 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.loginButton.setOnClickListener {
-            findNavController().navigate(R.id.action_LoginFragment_to_ProfileFragment)
+            loadLoginFragment()
+        }
+    }
+
+    private fun loadLoginFragment() {
+        requireActivity().supportFragmentManager.beginTransaction().apply {
+            replace(R.id.loginContainerView, LoginFragment())
+            commit()
         }
     }
 }
