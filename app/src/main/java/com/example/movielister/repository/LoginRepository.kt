0cd01d6a32939e006c4b7f2a4b86a600557a7535
akbasberk <53055798+berkakbas@users.noreply.github.com>
2023-01-10 13:07:34 +0300

    login handled by activity & token stored as liveData

--- app/src/main/java/com/example/movielister/repository/LoginRepository.kt
+++ app/src/main/java/com/example/movielister/repository/LoginRepository.kt
@@ -1,35 +1,37 @@
 package com.example.movielister.repository
 
+import androidx.lifecycle.MutableLiveData
 import com.example.movielister.api.LoginNetwork
 import com.example.movielister.model.SessionModel
 import com.example.movielister.model.TokenModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 class LoginRepository {
-    val _token = MutableSharedFlow<TokenModel>()
+    val token: MutableLiveData<TokenModel> by lazy {
+        MutableLiveData<TokenModel>()
+    }
 
-    val _sessionModel = MutableSharedFlow<SessionModel>()
+    var _sessionModel: MutableSharedFlow<SessionModel>? = null
 
-    val _loginToken = MutableSharedFlow<TokenModel>()
+    var _loginToken: MutableSharedFlow<TokenModel>? = null
 
     suspend fun createToken() {
         LoginNetwork.loginService.createToken()?.let {
-            val token = it
-            _token.emit(token)
+            token.value = it
         }
     }
 
     suspend fun createSession(token: String) {
         LoginNetwork.loginService.createSession(token)?.let {
             val sessionModel = it
-            _sessionModel.emit(sessionModel)
+            _sessionModel?.emit(sessionModel)
         }
     }
 
     suspend fun createSessionWithLogin(username: String, password: String, token: String) {
         LoginNetwork.loginService.createSessionWithLogin(username, password, token)?.let {
             val loginToken = it
-            _loginToken.emit(loginToken)
+            _loginToken?.emit(loginToken)
         }
     }
 }
