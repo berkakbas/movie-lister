64b0829ae38613f353575226517bb69b87980ac4
akbasberk <53055798+berkakbas@users.noreply.github.com>
2023-01-12 17:58:47 +0300

    splash screen implemented

---
+++ app/src/main/java/com/example/movielister/welcome/WelcomeActivity.kt
@@ -0,0 +1,22 @@
+package com.example.movielister.welcome
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.movielister.R
+import com.example.movielister.main.MainActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+    }
+}
