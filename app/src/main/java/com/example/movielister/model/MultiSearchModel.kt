76d59f33469aad312394e321cbd31cbbe27254e1
akbasberk <53055798+berkakbas@users.noreply.github.com>
2023-02-02 09:51:49 +0000

    multi search with movie details

--- app/src/main/java/com/example/movielister/model/MultiSearchModel.kt
+++ app/src/main/java/com/example/movielister/model/MultiSearchModel.kt
@@ -1,30 +1,33 @@
 package com.example.movielister.model
 
 import com.google.gson.annotations.SerializedName
-import com.squareup.moshi.JsonClass
-@JsonClass(generateAdapter = true)
-sealed class MultiSearchModel: BaseResponseModel() {
+import java.io.Serializable
+
+sealed class MultiSearchModel : Serializable {
     companion object {
         const val imageURL: String = "https://image.tmdb.org/t/p/original/"
     }
 
     val imageUrl get() = imageURL
 
-    @JsonClass(generateAdapter = true)
     data class MovieSearchModel(
         @SerializedName("id") val id: Int?,
         @SerializedName("title") val title: String?,
         @SerializedName("poster_path") val poster_path: String?,
+        @SerializedName("overview") val overview: String?,
+        @SerializedName("vote_average") val vote_average: Float?,
+        @SerializedName("release_date") val release_date: String?,
+        @SerializedName("genre_ids") val genre_ids: List<Int>?,
         @SerializedName("media_type") val mediaType: String?,
     ) : MultiSearchModel()
-    @JsonClass(generateAdapter = true)
+
     data class SeriesSearchModel(
         @SerializedName("id") val id: Int?,
         @SerializedName("name") val name: String?,
         @SerializedName("poster_path") val poster_path: String?,
         @SerializedName("media_type") val mediaType: String?,
     ) : MultiSearchModel()
-    @JsonClass(generateAdapter = true)
+
     data class PersonSearchModel(
         @SerializedName("id") val id: Int?,
         @SerializedName("name") val name: String?,
--- app/src/main/java/com/example/movielister/model/SearchResponseModel.kt
+++ app/src/main/java/com/example/movielister/model/SearchResponseModel.kt
@@ -2,8 +2,9 @@ package com.example.movielister.model
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import java.io.Serializable
 
 @JsonClass(generateAdapter = true)
 class SearchResponseModel(
     @Json(name = "results") val results: List<MultiSearchModel>
-): BaseResponseModel()
+): Serializable
